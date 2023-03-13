634b31031bac6bf09b2536f5c77e65a9d2d91557
Rodra11 <117209126+SamdevPT@users.noreply.github.com>
2023-03-13 17:34:07 +0000

    added total with modifier

--- dice/app.js
+++ dice/app.js
@@ -1,24 +1,30 @@
+//easing animation
+
 function easeOutQuart(x) {
     return 1 - Math.pow(1 - x, 4);
   }
 
 // Dice roller
-
  
   function rollDice(sides) {
     const resultElement = document.getElementById("result-d" + sides);
     const result = Math.floor(Math.random() * sides) + 1;
+    const modifier = parseInt(document.getElementById("modifier-d" + sides).value);
+    const resultTotalElement = document.getElementById("result-total-d" + sides); 
+    const resultTotal = result + modifier;
+
     resultElement.classList.remove("gold"); //remove the gold
     
-    // display random numbers for 1 second using an easing function
+    // display random numbers
     const startTime = Date.now();
     const intervalId = setInterval(function() {
     let  elapsedTime = Date.now() - startTime;
       if (elapsedTime >= 700) {
         clearInterval(intervalId);
-        resultElement.innerHTML = result;
+        resultElement.innerHTML = result; //dice roll
+        resultTotalElement.innerHTML = resultTotal //total with mod
         if (sides == 20 && result == sides) {
-            resultElement.classList.add("gold"); // add the "gold" class to the result element if the result is 20 on a d20
+            resultElement.classList.add("gold"); // add the "gold" class
           }
       } else {
         const progress = Math.min(1, elapsedTime / 700);
@@ -27,17 +33,20 @@ function easeOutQuart(x) {
         let  animatedNumber = Math.floor(randomNumber * easing);
         resultElement.innerHTML = animatedNumber;
       }
-    }, 50); // change this value to adjust the speed of the animation
+    }, 50); //speed of the animation
   }
   
 
 //   Clear results
   function clearResult() {
-    let  resultElements = document.getElementsByClassName("result");
-    for (let  i = 0; i < resultElements.length; i++) {
+    let resultElements = document.getElementsByClassName("result");
+    let resultTotalElements = document.getElementsByClassName("result-total");
+    for (let i = 0; i < resultElements.length; i++) {
       resultElements[i].innerHTML = "-";
+      resultTotalElements[i].innerHTML = "-";
     }
   }
+  
 
   //TODO - add modifier total. 
   
